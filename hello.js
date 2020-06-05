5a982022d9716232a540a507a15f313880edf421
allrude <allrude@gmail.com>
2020-06-05 12:58:03 +0200

    pending changes

--- hello.js
+++ hello.js
@@ -1,12 +1,3 @@
 import { helloWorld } from "https://raw.githubusercontent.com/allrude/deno-test/master/index.js?token=AAALSL6IACIXVNEKFDY2YL263ISDK";
-const s = serve({ port: 8000 });
-console.log("http://localhost:8000/");
-for await (const req of s) {
-  req.respond({
-    body: `
-  Hello World
-  is there a reason
-  Ruud van Zuidam
-  `,
-  });
-}
+console.log(Hello Ruud);
+
