ede95013ff249b3457f8fdbb52d0eaa4dfa398af
allrude <allrude@gmail.com>
2020-06-05 13:00:01 +0200

    pC

--- hello.js
+++ hello.js
@@ -1,2 +1,2 @@
 import { helloWorld } from "https://raw.githubusercontent.com/allrude/deno-test/master/index.js?token=AAALSL6IACIXVNEKFDY2YL263ISDK";
-console.log(helloWorld);
+console.log(helloWorld());
