f4eab52ea0c3205282f04e72d9e81646544985e3
allrude <allrude@gmail.com>
2020-06-05 12:58:47 +0200

    pennding changes

--- hello.js
+++ hello.js
@@ -1,3 +1,2 @@
 import { helloWorld } from "https://raw.githubusercontent.com/allrude/deno-test/master/index.js?token=AAALSL6IACIXVNEKFDY2YL263ISDK";
-console.log(Hello Ruud);
-
+console.log(helloWorld);
